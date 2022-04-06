622d8b087bfc8a66d0d5669df26bfe0c74d40f99
Lee JaeChan <toxic023@naver.com>
2022-04-06 16:35:54 +0900

    project

--- 국립현대미술관 클론/js/function.js
+++ 국립현대미술관 클론/js/function.js
@@ -1,6 +1,8 @@
 $(function () {
 
 
+  // 네비게이션
+
   let idx = 0;
 
 
@@ -10,14 +12,18 @@ $(function () {
 
   console.log($gnb.length)
   console.log($lnb.length)
+  if ($('html,body').scrollTop() == 40) {
+    $('article').css({ display: 'none' })
+
+  }
   $gnb.on('mouseenter', function () {
     idx = $gnb.index(this)
 
     $('.gnb').css({ borderBottom: 'none' })
     $gnb.not().css({ color: '#ddd' })
     $gnb.eq(idx).css({ color: '#000' })
-    $lnb.not().css({ display: 'none' })
-    $lnb.eq(idx).css({ display: 'block' })
+    $lnb.not().fadeOut(100)
+    $lnb.eq(idx).fadeIn(300)
     $('.shadow').css({ display: 'block' })
   })
 
@@ -25,11 +31,77 @@ $(function () {
     $lnb.css({ display: 'none' })
     $gnb.css({ color: '#000' })
     $('.gnb').css({ borderBottom: '1px solid #000' })
-    $('.shadow').css({ display: 'none' })
+    $('.shadow').fadeOut(100)
   })
 
   $search.on('click', function () {
-    $('.search-box').slideDown()
+
+    if ($search.hasClass('close')) {
+      $search.removeClass('close')
+      $('.search-box').slideUp()
+      $('.shadow').css({ display: 'none' })
+    } else {
+      $search.addClass('close')
+      $('.search-box').slideDown()
+      $('.shadow').css({ display: 'block' })
+    }
   })
 
+
+  // 그림자
+
+  // let shadowWidth = $('article').width();
+  // $('.shadow').css({
+  //   // width: shadowWidth
+  // })
+
+  // 알림창
+  $('section > .notice > a').on('click', function (evt) {
+    evt.preventDefault();
+
+    $('section > .notice').slideUp(300);
+  })
+
+
+
+  // 슬라이드
+
+  const $container = $('#exhibit > .slides > .slides-container')
+  const $next = $('#exhibit > .slides > .next')
+
+
+  let nowNum = 0;
+
+  $next.on('click', function (evt) {
+    evt.preventDefault();
+
+
+
+    if (nowNum < 2) {
+      nowNum++;
+    } else {
+      nowNum = 0
+    }
+    $container.stop().animate({
+      left: -300 * nowNum
+    })
+  })
+
+
+
+
+  $('aside > a > i').on('click', function (evt) {
+    evt.preventDefault();
+
+    $('html,body').animate({
+      scrollTop: 0
+    })
+  })
+
+
+
+
+
 })
+
+
