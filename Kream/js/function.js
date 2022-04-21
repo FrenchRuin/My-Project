827c2d9c5fbaadf2a6d0e06487e86abd6cfd7c57
Lee JaeChan <toxic023@naver.com>
2022-04-21 18:15:08 +0900

    project

--- Kream/js/function.js
+++ Kream/js/function.js
@@ -51,4 +51,52 @@ $(function () {
       $nextBtn.trigger('click')
     }, 3000)
   })
+
+
+  //STYLE PICKS 슬라이드
+
+  const $style_slides = $('#stylePicks > .slides > .slides-container')
+  const $style_slides_nextBtn = $('#stylePicks > a.next')
+
+
+  let style_slides_Idx = 0;
+  $style_slides_nextBtn.on('click', function (evt) {
+    evt.preventDefault();
+
+    if (style_slides_Idx < 4) {
+      style_slides_Idx++;
+
+    } else {
+      style_slides_Idx = 0;
+
+    }
+
+    $style_slides.stop().animate({
+      left: (-200 * style_slides_Idx) + 'px'
+    })
+
+  })
+  //Daily Looks 슬라이드
+
+  const $daily_slides = $('#dailylook > .slides > .slides-container')
+  const $daily_slides_nextBtn = $('#dailylook > a.next')
+
+
+  let daily_slides_Idx = 0;
+  $daily_slides_nextBtn.on('click', function (evt) {
+    evt.preventDefault();
+
+    if (daily_slides_Idx < 4) {
+      daily_slides_Idx++;
+
+    } else {
+      daily_slides_Idx = 0;
+
+    }
+
+    $daily_slides.stop().animate({
+      left: (-200 * daily_slides_Idx) + 'px'
+    })
+
+  })
 })
