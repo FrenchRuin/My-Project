12cd149063fd639cb2037b7bc92ba23d3d69c017
Lee JaeChan <toxic023@naver.com>
2022-05-02 17:53:32 +0900

    project

--- kia/js/function.js
+++ kia/js/function.js
@@ -21,6 +21,8 @@ $(function () {
   }
 
 
+
+
   $(window).on('scroll', function () {
 
 
@@ -56,6 +58,11 @@ $(function () {
     slideFn();
   })
 
+  $(window).on('load', function () {
+    setInterval(function () {
+      $next.trigger('click')
+    }, 5000)
+  })
 
 
 
@@ -72,4 +79,52 @@ $(function () {
 
     $autoBtn.children('i').toggleClass('fa-solid fa-circle-pause')
   })
+
+
+
+  //bestkia 슬라이드
+
+  const $bestSlides = $('#bestkia > .slides > .slides-container')
+
+  const $bestNext = $('.best_next')
+  const $bestPrev = $('.best_prev')
+
+  let bestIdx = 0;
+
+  $bestNext.on('click', function (evt) {
+    evt.preventDefault();
+
+
+
+
+
+    $bestSlides.stop().animate({
+      left: '-800px'
+    }, 400, function () {
+      $bestSlides.css({
+        left: '-300px'
+      })
+      $('#bestkia > .slides > .slides-container > .slide').first().appendTo($bestSlides)
+
+
+    })
+  })
+  $bestPrev.on('click', function (evt) {
+    evt.preventDefault();
+
+    $bestSlides.stop().animate({
+      left: '200px'
+    }, 400, function () {
+      $bestSlides.css({
+        left: '-300px'
+      })
+      $('#bestkia > .slides > .slides-container > .slide').last().prependTo($bestSlides)
+
+    })
+  })
+
+
+
+
+
 })
